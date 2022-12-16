2f7ad3036362e150abb660d960d103f7226f6c02
Gerrit <info@gerritschwerthelm.de>
2022-12-16 16:06:38 +0100

    Cleanup old tests.

--- tests/control_plane/control_plane_helper.py
+++
@@ -1,7 +0,0 @@
-from tests.ansible_helper import AnsibleTest
-from tests.control_plane import PLAYBOOK_DIR, INVENTORY_DIR
-
-
-class AnsibleTestControlPlane(AnsibleTest):
-    def load_ansible_role(self, inventory_name, playbook_name, play_name, role_name):
-        self.load(INVENTORY_DIR, inventory_name, PLAYBOOK_DIR, playbook_name, play_name, role_name)
