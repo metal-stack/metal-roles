4147f1323904b7001e05d1a8832416e1a53ed7c1
Markus Fensterer <markus.fensterer@gmail.com>
2023-02-06 17:37:04 +0100

    data driven tests for metal.yaml and frr.conf rendering and finished work on interconnects

---
+++ partition/roles/sonic/test/template_test.py
@@ -0,0 +1,37 @@
+import unittest
+
+import yaml
+import os
+from test import read_template_file
+
+from ansible.template import Templar
+
+def readYaml(f):
+    with open(os.path.join(os.path.dirname(__file__), f), "r") as stream:
+        return yaml.safe_load(stream)
+
+def readFile(f):
+    with open(os.path.join(os.path.dirname(__file__), f), "r") as stream:
+        return stream.read()
+
+class SonicRoleTemplates(unittest.TestCase):
+    def test_sonic_role_templates(self):
+        self.maxDiff = None
+        defaults = readYaml('../defaults/main.yaml')
+        metal_t = read_template_file('metal.yaml.j2')
+        frr_t = read_template_file('frr.conf.j2')
+
+        for tc in next(os.walk(os.path.join(os.path.dirname(__file__), 'data')))[1]:
+            if tc.startswith("."):
+                continue
+
+            vars = defaults | readYaml(f'./data/{tc}/input.yaml')
+            templar = Templar(loader=None, variables=vars)
+            
+            metal_exp = readFile(f'./data/{tc}/metal.yaml')
+            metal_res = templar.template(metal_t)
+            self.assertEqual(metal_exp.strip(), metal_res.strip(), 'detected a diff for metal.yaml rendering')
+            
+            frr_exp = readFile(f'./data/{tc}/frr.conf')
+            frr_res = templar.template(frr_t)
+            self.assertEqual(frr_exp.strip(), frr_res.strip(), 'detected a diff for frr.conf rendering')
