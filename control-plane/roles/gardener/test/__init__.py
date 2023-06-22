2492ec112c049e685be653f8fd31f89a499e85f6
Gerrit <info@gerritschwerthelm.de>
2023-06-22 13:49:37 +0200

    Adding a test.

--- control-plane/roles/gardener/test/__init__.py
+++ control-plane/roles/gardener/test/__init__.py
@@ -6,3 +6,8 @@ FILTER_PLUGINS_PATH = os.path.join(os.path.dirname(os.path.abspath(os.path.dirna
 def read_mock_file(name):
     with open(os.path.join(os.path.dirname(__file__), "mock", name), 'r') as f:
         return f.read()
+
+
+def read_template_file(name):
+    with open(os.path.join(os.path.dirname(__file__), "..", "templates", name), 'r') as f:
+        return f.read()
