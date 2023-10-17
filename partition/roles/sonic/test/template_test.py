2ceb8a35d89a43717de4f5b7afdc5658266fcadf
Robert Volkmann <robert.volkmann@x-cellent.com>
2023-10-17 09:16:17 +0200

    Split frr.conf.j2 into multiple files

--- partition/roles/sonic/test/template_test.py
+++ partition/roles/sonic/test/template_test.py
@@ -2,9 +2,10 @@ import unittest
 
 import yaml
 import os
-from test import read_template_file
 
 from ansible.template import Templar
+
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "..", "templates")
 
 
 def readYaml(f):
@@ -17,12 +18,17 @@ def readFile(f):
         return stream.read()
 
 
+def read_template_file(name):
+    with open(os.path.join(TEMPLATE_DIR, name), 'r') as f:
+        return f.read()
+
+
 class SonicRoleTemplates(unittest.TestCase):
     def test_sonic_role_templates(self):
         self.maxDiff = None
         defaults = readYaml('../defaults/main.yaml')
         metal_t = read_template_file('metal.yaml.j2')
-        frr_t = read_template_file('frr.conf.j2')
+        frr_t = read_template_file('frr/base.j2')
         iptables_t = read_template_file('iptables.json.j2')
 
         for tc in next(os.walk(os.path.join(os.path.dirname(__file__), 'data')))[1]:
@@ -37,10 +43,11 @@ class SonicRoleTemplates(unittest.TestCase):
             self.assertEqual(metal_exp.strip(), metal_res.strip(), 'detected a diff for metal.yaml rendering - tc ' + tc)
 
             frr_exp = readFile(f'./data/{tc}/frr.conf')
-            frr_res = templar.template(frr_t)
+            with templar.set_temporary_context(searchpath=[TEMPLATE_DIR]):
+                frr_res = templar.template(frr_t)
             self.assertEqual(frr_exp.strip(), frr_res.strip(), 'detected a diff for frr.conf rendering - tc ' + tc)
 
             if 'sonic_extended_cacl' in vars:
                 iptables_exp = readFile(f'./data/{tc}/iptables.json')
-                iptables_res = templar.template(iptables_t,convert_data=False)
+                iptables_res = templar.template(iptables_t, convert_data=False)
                 self.assertEqual(iptables_exp.strip(), iptables_res.strip(), 'detected a diff for iptables.json rendering - tc ' + tc)
