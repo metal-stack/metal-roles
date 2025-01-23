07e321a01f99dd4ad59575111be461e95534b260
Stefan Majer <stefan.majer@f-i-ts.de>
2025-01-23 12:50:56 +0100

    Do not delete destination file

--- partition/roles/metal-core/files/bgp-neighbor-stats.sh
+++ partition/roles/metal-core/files/bgp-neighbor-stats.sh
@@ -7,6 +7,5 @@ destfile=/var/run/bgp-neighbors/bgp-neighbors.json
 
 /usr/bin/vtysh -c "show ip bgp vrf all neighbors json" > "${tmpfile}"
 
-rm -f "${destfile}"
 mv "${tmpfile}" "${destfile}"
 rm -f "${tmpfile}"
