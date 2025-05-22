e8fb33df8fe993599f7da8c6c0ce9463e315fe13
Ilja Rotar <kontakt@ilja-rotar.de>
2025-05-22 16:57:55 +0200

    integrate frr.conf changes into new sonic-config role

--- partition/roles/sonic-config/test/template_test.py
+++ partition/roles/sonic-config/test/template_test.py
@@ -26,13 +26,18 @@ class SonicConfigRoleTemplates(unittest.TestCase):
         frr_t = read_template_file('frr.conf.j2')
         iptables_t = read_template_file('iptables.json.j2')
 
-        vars = defaults | read_yaml('./data/input.yaml')
-        templar = Templar(loader=None, variables=vars)
+        for tc in next(os.walk(os.path.join(os.path.dirname(__file__), 'data')))[1]:
+            if tc.startswith("."):
+                continue
 
-        frr_exp = read_file('./data/frr.conf')
-        frr_res = templar.template(frr_t)
-        self.assertEqual(frr_exp.strip(), frr_res.strip(), 'frr.conf differs from the expected result')
+            vars = defaults | read_yaml(f'./data/{tc}/input.yaml')
+            templar = Templar(loader=None, variables=vars)
 
-        iptables_exp = read_json('./data/iptables.json')
-        iptables_res = templar.template(iptables_t)
-        self.assertEqual(iptables_exp, iptables_res, 'iptables.json differs from the expected result')
+            frr_exp = read_file(f'./data/{tc}/frr.conf')
+            frr_res = templar.template(frr_t)
+            self.assertEqual(frr_exp.strip(), frr_res.strip(), 'frr.conf differs from the expected result')
+
+            if 'sonic_config_extended_cacl' in vars:
+                iptables_exp = read_json(f'./data/{tc}/iptables.json')
+                iptables_res = templar.template(iptables_t)
+                self.assertEqual(iptables_exp, iptables_res, 'iptables.json differs from the expected result')
